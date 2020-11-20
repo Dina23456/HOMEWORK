1a84ef8591d06a490406b4c0561ad02dbac1fa26
Dina23456 <dianadem1606@gmail.com>
2020-11-20 14:32:24 +0200

    h13

--- h13/h13.js
+++ h13/h13.js
@@ -8,121 +8,123 @@
 //   Якщо щось пішло не так (нема шо їсти), то має бути викинута помилка і решта функцій виконуватись не мають.
 //   Якщо ж все ок, то ви маєте прожити свій звичайний день.
 //   Кожна подія має бути з рандомною (не по зростанню) затримкою.
-// function day(hour) {
-//   return new Promise((resolve, reject) => {
-//     console.log('Morning');
-//     console.log('Alarm ......');
-//
-//     setTimeout(() => {
-//       console.log('Brbrbrbrbrbrbrbrb');
-//
-//       if (hour > 7 && hour < 9) {
-//         resolve('wake up');
-//       } else {
-//         reject('sleep on');
-//       }
-//     }, 1000);
-//   })
-// }
-//
-// function breakfast() {
-//   return new Promise((resolve) => {
-//     console.log("found food");
-//     setTimeout(() => {
-//       console.log('I had breakfast');
-//       resolve('brush your teeth');
-//     }, 500)
-//   })
-// }
-//
-// function gostudy(bus) {
-//   return new Promise((resolve, reject) => {
-//     console.log("go to study");
-//     setTimeout(() => {
-//       if (bus === true) {
-//         resolve('go');
-//       } else {
-//         reject('take the taxi');
-//       }
-//     }, 2000);
-//   })
-// }
-//
-// function afterstudy(hours) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       if (hours > 12 && hours < 14) {
-//         resolve('go to work');
-//       } else {
-//         reject('rest');
-//       }
-//     }, 500);
-//   })
-// }
-//
-// function afterjob() {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       console.log('I have to buy products');
-//       resolve('go to shop');
-//     }, 5000);
-//   })
-// }
-//
-// function shop(money) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       if (money > 50) {
-//         resolve('buy products');
-//       } else {
-//         reject('if do not money go to home');
-//       }
-//     }, 500);
-//   })
-// }
-//
-//
-// day(8)
-//   .then((next) => {
-//     console.log(`Again early ${next}`);
-//     return breakfast();
-//   })
-//   .then(nextstep => {
-//     console.log(nextstep);
-//     return gostudy(true)
-//   })
-//   .then((nextstep2) => {
-//     console.log(`Go go ${nextstep2}`);
-//     return afterstudy(13)
-//   })
-//   .then((nextstep3) => {
-//     console.log(nextstep3);
-//     return afterjob();
-//   })
-//   .then((step) => {
-//     console.log(step);
-//     return shop(20)
-//   })
-//   .then((step2) => {
-//     console.log(step2);
-//   })
-//   .catch(err => {
-//     console.log('___________________')
-//     console.log(err)
-//     console.log('___________________')
-//   })
-//   .finally(() => {
-//     console.log('rest');
-//   })
+function day(hour) {
+  return new Promise((resolve, reject) => {
+    console.log('Morning');
+    console.log('Alarm ......');
 
+    setTimeout(() => {
+      console.log('Brbrbrbrbrbrbrbrb');
+
+      if (hour > 7 && hour < 9) {
+        resolve('wake up');
+      } else {
+        reject('sleep on');
+      }
+    }, 1000);
+  })
+}
+
+function breakfast() {
+  return new Promise((resolve) => {
+    console.log("found food");
+    setTimeout(() => {
+      console.log('I had breakfast');
+      resolve('brush your teeth');
+    }, 500)
+  })
+}
+
+function gostudy(bus) {
+  return new Promise((resolve, reject) => {
+    console.log("go to study");
+    setTimeout(() => {
+      if (bus === true) {
+        resolve('go');
+      } else {
+        reject('take the taxi');
+      }
+    }, 2000);
+  })
+}
+
+function afterstudy(hours) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (hours > 12 && hours < 14) {
+        resolve('go to work');
+      } else {
+        reject('rest');
+      }
+    }, 500);
+  })
+}
+
+function afterjob() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('I have to buy products');
+      resolve('go to shop');
+    }, 5000);
+  })
+}
+
+function shop(money) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (money > 50) {
+        resolve('buy products');
+      } else {
+        reject('if do not money go to home');
+      }
+    }, 500);
+  })
+}
+
+
+day(8)
+  .then((next) => {
+    console.log(`Again early ${next}`);
+    return breakfast();
+  })
+  .then(nextstep => {
+    console.log(nextstep);
+    return gostudy(true)
+  })
+  .then((nextstep2) => {
+    console.log(`Go go ${nextstep2}`);
+    return afterstudy(13)
+  })
+  .then((nextstep3) => {
+    console.log(nextstep3);
+    return afterjob();
+  })
+  .then((step) => {
+    console.log(step);
+    return shop(20)
+  })
+  .then((step2) => {
+    console.log(step2);
+  })
+  .catch(err => {
+    console.log('___________________')
+    console.log(err)
+    console.log('___________________')
+  })
+  .finally(() => {
+    console.log('rest');
+  })
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 function day(hour, task) {
   console.log('Morning');
   console.log('Alarm ......');
   setTimeout(() => {
 
     console.log('Brbrbrbrbrbrbrbrb');
-    if (hour>=7) {
-      task('wake up' );
+    if (hour >= 7) {
+      task('wake up');
       return;
     }
     task('sleep on');
@@ -134,7 +136,7 @@ function breakfast(task) {
   console.log("found food");
   setTimeout(() => {
     console.log('I had breakfast');
-    task( 'brush your teeth');
+    task('brush your teeth');
   }, 500)
 }
 
@@ -143,7 +145,7 @@ function gostudy(bus, task) {
   setTimeout(() => {
 
     if (bus === true) {
-      task( 'go');
+      task('go');
       return
     }
     task('take the taxi');
@@ -153,7 +155,7 @@ function gostudy(bus, task) {
 function afterstudy(hours, task) {
   setTimeout(() => {
 
-    if (hours>12 && hours<14) {
+    if (hours > 12 && hours < 14) {
       task('go to work');
       return;
     }
@@ -170,31 +172,31 @@ function afterjob(task) {
 
 function shop(money, task) {
   setTimeout(() => {
-    if (money>50) {
+    if (money > 50) {
       task('buy products');
       return;
     }
-    task( 'if you do not money go to home');
+    task('if you do not money go to home');
   }, 500);
 }
 
 day(9, (err) => {
-    console.log(`Again early ${err}`);
-    breakfast((step) =>{
-        console.log(step);
-        gostudy(true, (err) => {
+  console.log(`Again early ${err}`);
+  breakfast((step) => {
+    console.log(step);
+    gostudy(true, (err) => {
+      console.log(err);
+      afterstudy(13, (err) => {
+        console.log(err);
+        afterjob((arr) => {
+          console.log(arr);
+          shop(200, (err) => {
             console.log(err);
-            afterstudy(13, (err) => {
-                console.log(err);
-                afterjob((arr) => {
-                    console.log(arr);
-                    shop(200, (err) => {
-                        console.log(err);
-                    })
-                })
-            })
+          })
         })
+      })
     })
+  })
 })
 
 
