40b4aba4d5b2bf3896860a7129c4aed4bbb977d3
Dina23456 <dianadem1606@gmail.com>
2020-11-23 17:46:33 +0200

    до 14

--- H14/H14.js
+++ H14/H14.js
@@ -129,22 +129,4 @@
 //     })
 // })}}
 //
-let hello = document.getElementById('hello')
-let btn = document.getElementById('btn')
-let write = document.getElementById('write');
 
-btn.onclick = async() => {
-  write.innerText = "";
-  for (let i = 0; i < hello.value.length; i++)
-    await arrey(hello.value[i])
-}
-
-function arrey(string){
-  return new Promise(resolve => {
-    setTimeout(() => {
-      console.log(string)
-      write.innerText += string
-      resolve()
-    }, Math.random() * (1001 - 1) + 1);
-  })
-}
